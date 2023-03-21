a12b86663aa9cec430c371e3bb862fbc8f0f767b
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-03-22 05:42:07 +0700

    fix mainprogram and dfs algo

---
+++ src/DFSalgorithm.cs
@@ -0,0 +1,80 @@
+using System;
+using ExplorerState;
+using MazeMap;
+using Game;
+
+namespace DFSalgorithm
+{
+    // DFSalgorithm Class bertanggung jawab atas setiap aksi perpindahan dalam penelusuran dengan algoritma Depth-First-Search
+    // DFSalgorithm Class adalah kelas turunan dari ExplorerAction Class
+    class DFS : ExplorerAction
+    {
+        /* Method */
+        // Constructor
+        public DFS(Maze maze) : base(maze) { }
+        // Implementasi method untuk backtracking saat tidak ada lagi adjacent node yang dapat dikunjungi
+        public void backTrack()
+        {
+            switch (this.route.Last())
+            {
+                case 'R':
+                    goToLeft();
+                    break;
+                case 'L':
+                    goToRight();
+                    break;
+                case 'U':
+                    goToDown();
+                    break;
+                case 'D':
+                    goToUp();
+                    break;
+            }
+        }
+        // Pemilihan aksi perpindahan/pergerakan setiap waktu dengan algoritma Depth-First-Search
+        public override void setCurrentAction(Maze maze, GameState game)
+        {
+            depthFirstSearch(this.firstPos, maze, game);
+        }
+        // Implementasi algoritma depth-first-search
+        public void depthFirstSearch(Position pos, Maze maze, GameState game)
+        {
+            if (game.getTreasureCount() > 0)
+            {
+                Visit(pos.getY(), pos.getX());
+                if (maze.getMapElement(pos.getY(), pos.getX()) == GameState.TREASURE_PLACE)
+                {
+                    // Ditemukan Treasure
+                    maze.setMapElement('R', pos.getY(), pos.getX());
+                    game.setTreasureCount(game.getTreasureCount() - 1);
+                    if (game.getTreasureCount() == 0) return;
+                }
+                if (isAllAdjVisited())
+                {
+                    // Semua adjacent node sudah dikunjungi, lakukan backtracking
+                    backTrack();
+                }
+                else
+                {
+
+                    // Prioritas Perpindahan : Kiri, Bawah, Kanan, Atas
+                    if (isLeftVisitable()) goToLeft();
+                    else if (isDownVisitable()) goToDown();
+                    else if (isRightVisitable()) goToRight();
+                    else if (isUpVisitable()) goToUp();
+                }
+                depthFirstSearch(this.getCurrentPosition(), maze, game);
+            }
+        }
+        // Setup awal kondisi penelusuran untuk kembali ke titik awal (TSP Problem)
+        // Setup dilakukan dengan mengubah posisi awal explorer 'K' menjadi tujuan akhir/treasure 'T' serta posisi saat ini menjadi posisi awal 'K'
+        // Setelah setup dilakukan, akan diimplementasikan kembali skema DFS yang sama pada langkah penemuan treasure
+        public void TSPSetupDFS(Position currentPos, Maze maze, GameState game)
+        {
+            maze.setMapElement('T', firstPos.getY(), firstPos.getX());
+            maze.setMapElement('K', currentPos.getY(), currentPos.getX());
+            game.setTreasureCount(1);
+            setInitVisitedMap(maze);
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,16 +1,18 @@
 using System;
 using MazeMap;
 using Game;
-using PlayerGame;
+using ExplorerState;
 using DFSalgorithm;
 using BFSalgorithm;
 
 namespace TubesHunting
 {
+    // Program Class bertanggung jawab sebagai main program untuk menjalankan penelusuran
     class Program
     {
         static void Main(string[] args)
         {
+            // Input file and validation
             Console.Write("Masukkan nama file : ");
             string? fileName = Console.ReadLine();
             while (fileName == null)
@@ -19,10 +21,9 @@ namespace TubesHunting
                 Console.Write("Masukkan nama file : ");
                 fileName = Console.ReadLine();
             }
-
             string filePath = "../test/" + fileName + ".txt";
 
-            // Map matriks
+            // Inisiasi peta/map dalam bentuk matriks
             Maze mazeMap = new MazeMap.Maze();
             mazeMap.setCols(filePath);
             mazeMap.setRows(filePath);
@@ -30,7 +31,7 @@ namespace TubesHunting
             mazeMap.setMapMatrix(mapTemp.getMapMatrix());
             while (mazeMap.getCols() == 0)
             {
-                Console.WriteLine("File Not Found! Try Again");
+                Console.WriteLine("File Not Found! Pastikan file berada pada folder test dalam bentuk .txt");
                 Console.Write("Masukkan nama file : ");
                 fileName = Console.ReadLine();
                 filePath = "../test/" + fileName + ".txt";
@@ -39,42 +40,66 @@ namespace TubesHunting
                 mapTemp = new MazeMap.Maze(filePath, mazeMap.getRows(), mazeMap.getCols());
                 mazeMap.setMapMatrix(mapTemp.getMapMatrix());
             }
+
+            // Mencetak matriks untuk debugging
+            Console.WriteLine("Input map :");
             mazeMap.printMap(mazeMap.getMapMatrix());
+
+            // Inisiasi state permainan/penelusuran
             GameState game = new GameState(mazeMap.getMapMatrix());
             if (game.getTreasureCount() <= 0)
             {
+                // Kondisi: tidak ada treasure pada map, tidak perlu dilakukan penelusuran
                 Console.WriteLine("There's no treasure!");
             }
             else
             {
-                Console.WriteLine("Treasure : " + game.getTreasureCount());
-                Console.Write("BFS or DFS (B/D)? ");
+                // Pemilihan Algoritma penelusuran
+                Console.Write("Pilih Algoritma Penelusuran (BFS/DFS) : ");
                 string? ans = Console.ReadLine();
-                if (ans == "B")
+                while (ans != "BFS" && ans != "bfs" && ans != "DFS" && ans != "dfs")
                 {
+                    Console.WriteLine("Masukan tidak valid. Silakan ulangi masukan");
+                    Console.Write("Pilih Algoritma Penelusuran (BFS/DFS) : ");
+                    ans = Console.ReadLine();
+                }
+
+                if (ans == "BFS" || ans == "bfs")
+                {
+                    // Lakukan penelusuran dengan Breadth-First-Search
                     BFS b = new BFS(mazeMap);
                     b.setCurrentAction(mazeMap, game);
                 }
-                else if (ans == "D")
+                else if (ans == "DFS" || ans == "dfs")
                 {
+                    // Lakukan penelusuran dengan Breadth-First-Search
                     DFS d = new DFS(mazeMap);
                     d.setCurrentAction(mazeMap, game);
-                    Console.WriteLine("=== Route ====");
-                    d.setCurrentAction(mazeMap, game);
-                    Console.Write("Route: ");
-                    d.printRoute();
-                    Console.WriteLine();
 
-                    Console.WriteLine("=== Route ====");
-                    d.TSPSetupDFS(d.getCurrentPosition(), mazeMap, game);
-                    Console.WriteLine("First Pos (" + d.getFirstPosition().getX() + "," + d.getFirstPosition().getY() + ")");
-                    d.setCurrentAction(mazeMap, game);
-                    Console.WriteLine();
+                    Console.Write("Lanjutkan hingga kembali ke titik awal : TSP (Y/N)? ");
+                    ans = Console.ReadLine();
+                    while (ans != "Y" && ans != "y" && ans != "N" && ans != "n")
+                    {
+                        Console.WriteLine("Masukan tidak valid. Silakan ulangi masukan");
+                        Console.Write("Lanjutkan hingga kembali ke titik awal : TSP (Y/N)? ");
+                        ans = Console.ReadLine();
+                    }
+
+                    if (ans == "Y" || ans == "y")
+                    {
+                        // Penelusuran kembali ke titik awal
+                        d.TSPSetupDFS(d.getCurrentPosition(), mazeMap, game);
+                        d.setCurrentAction(mazeMap, game);
+                    }
+
+                    // Mencetak informasi penelusuran
                     Console.WriteLine("Treasure found in " + d.getRoute().Count + " steps!");
-                    //Console.WriteLine("Nodes: " + b.getNodeVisitedCount());
                     Console.Write("Route: ");
                     d.printRoute();
-                    Console.WriteLine();
+                    Console.WriteLine("Visited Node Route: ");
+                    d.printCoorVisited();
+                    d.setNodes(d.countNodes());
+                    Console.WriteLine("Node: " + d.getNodes());
                 }
             }
         }
