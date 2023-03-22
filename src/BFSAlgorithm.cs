83cfe3cdb4833ce9b75ca46424c75bf94d86a078
Putinabillaa <aidiraputiabil@gmail.com>
2023-03-22 20:07:23 +0700

    merging repo fails, manual re-commit instead

---
+++ src/BFSAlgorithm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using ExplorerState;
+using MazeMap;
+using Game;
+
+namespace BFSalgorithm
+{
+    // BFSalgorithm Class bertanggung jawab atas setiap aksi perpindahan dalam penelusuran dengan algoritma Breadth-First-Search
+    // BFSalgorithm Class adalah kelas turunan dari ExplorerAction Class
+    class BFS : ExplorerAction
+    {
+        /* Method */
+        // Constructor
+        public BFS(Maze maze) : base(maze) { }
+        // Pemilihan aksi perpindahan/pergerakan setiap waktu dengan algoritma Breadth-First-Search
+        public override void setCurrentAction(Maze maze, GameState game)
+        {
+            breadthFirstSearch(this.firstPos, maze, game);
+        }
+        // Implementasi algoritma breadth-first-search
+        public void breadthFirstSearch(Position pos, Maze maze, GameState game)
+        {
+            // Prioritas Belok : Kiri, Bawah, Kanan, Atas
+            int[] dx = { -1, 0, 1, 0 };
+            int[] dy = { 0, 1, 0, -1 };
+            bool[,] onQueue = new bool[maze.getRows(), maze.getCols()];
+
+            int startX = pos.getX();
+            int startY = pos.getY();
+
+            Queue<(int x, int y, List<char> route)> q = new Queue<(int, int, List<char>)>();
+            q.Enqueue((startX, startY, this.getRoute()));
+            onQueue[startY, startX] = true;
+
+            if (startX == getFirstPosition().getX() && startY == getFirstPosition().getY())
+            {
+                Visit(startY, startX);
+            }
+
+            while (q.Count > 0)
+            {
+                var current = q.Dequeue();
+
+                Position currentPos = new Position(current.x, current.y);
+                this.setCurrentPosition(currentPos);
+                this.setRoute(current.route);
+
+                if (current.y != startY || current.x != startX)
+                {
+                    Visit(current.y, current.x);
+                }
+
+                if (maze.getMapElement(currentPos.getY(), currentPos.getX()) == GameState.TREASURE_PLACE)
+                {
+                    maze.setMapElement(GameState.ROAD, currentPos.getY(), currentPos.getX());
+                    game.setTreasureCount(game.getTreasureCount() - 1);
+                    break;
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    int newX = currentPos.getX() + dx[i];
+                    int newY = currentPos.getY() + dy[i];
+
+                    if (newX < 0 || newY >= maze.getRows() || newY < 0 || newX >= maze.getCols())
+                    {
+                        continue;
+                    }
+
+                    if (maze.getMapElement(newY, newX) == GameState.OBSTACLES
+                        || onQueue[newY, newX])
+                    {
+                        continue;
+                    }
+
+                    List<char> newRoute = new List<char>(current.route);
+                    switch (i)
+                    {
+                        case 0:
+                            newRoute.Add('L');
+                            break;
+                        case 1:
+                            newRoute.Add('D');
+                            break;
+                        case 2:
+                            newRoute.Add('R');
+                            break;
+                        case 3:
+                            newRoute.Add('U');
+                            break;
+                    }
+                    onQueue[newY, newX] = true;
+                    q.Enqueue((newX, newY, newRoute));
+                }
+            }
+            if (game.getTreasureCount() > 0)
+            {
+                breadthFirstSearch(getCurrentPosition(), maze, game);
+            }
+        }
+        // Implementasi penelusuran untuk kembali ke titik awal (TSP Problem) dengan algoritma Breadth-First-Search
+        public void runTSPforBFS(Position currentPos, Maze maze, GameState game)
+        {
+            maze.setMapElement('T', firstPos.getY(), firstPos.getX());
+            maze.setMapElement('K', currentPos.getY(), currentPos.getX());
+            game.setTreasureCount(1);
+            breadthFirstSearch(currentPos, maze, game);
+        }
+    }
+}
---
+++ src/MazeMap.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+
+namespace MazeMap
+{
+    public class Maze
+    {
+        // Attributes
+        public char[][] mapMatrix = { };
+        private int rows;
+        private int cols;
+        private int countK;
+        private int countT;
+        // Default folder
+        public static readonly string rootFolder = "../test/";
+        public Maze()
+        {
+            this.cols = 0;
+            this.rows = 0;
+            this.countK = 0;
+            this.countT = 0;
+
+        }
+        public Maze(string filePath, int rows, int cols)
+        {
+            this.countK = 0;
+            this.countT = 0;
+            if (File.Exists(filePath))
+            {
+                // Initialize matrix
+                string[] lines = File.ReadAllLines(filePath);
+                this.mapMatrix = new char[rows][];
+                int i, j;
+                for (i = 0; i < this.mapMatrix.Length; i++)
+                    this.mapMatrix[i] = new char[cols];
+
+                // Read a text file line by line.
+                i = 0;
+                foreach (string line in lines)
+                {
+                    char[] charPerLine = line.ToCharArray();
+                    j = 0;
+                    foreach (char huruf in charPerLine)
+                    {
+                        if (huruf != ' ')
+                        {
+                            // Console.WriteLine("this [" + i + ", " + j + "] = " + huruf);
+                            Console.WriteLine(huruf);
+                            if (huruf == 'K') countK += 1;
+                            else if (huruf == 'T') countT += 1;
+                            else if(huruf != 'R' && huruf != 'X') throw new MazeException();
+                            Console.WriteLine(countK);
+                            Console.WriteLine(countT);
+                            this.mapMatrix[i][j] = huruf;
+                            j++;
+                        }
+                    }
+                    i++;
+                }
+            }
+        }
+        public void setRows(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                this.rows = lines.Length;
+            }
+            else this.rows = 0;
+        }
+        public void setCols(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                int j = 0;
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (char huruf in lines[0])
+                {
+                    if (huruf != ' ')
+                    {
+                        j++;
+                    }
+                }
+                this.cols = j;
+            }
+            else this.cols = 0;
+        }
+
+        public int getRows()
+        {
+            return this.rows;
+        }
+        public int getCols()
+        {
+            return this.cols;
+        }
+        public void setMapMatrix(char[][] mapMatrix)
+        {
+            this.mapMatrix = mapMatrix;
+        }
+        public char[][] getMapMatrix()
+        {
+            return this.mapMatrix;
+        }
+        public void setMapElement(char newElmt, int rows, int cols)
+        {
+            this.mapMatrix[rows][cols] = newElmt;
+        }
+        public char getMapElement(int rows, int cols)
+        {
+            return this.mapMatrix[rows][cols];
+        }
+        public void printMap(char[][] mapMatrix)
+        {
+            // Console.WriteLine("Lenght = " + mapMatrix.Length + " - " + mapMatrix[0].Length);
+            for (int i = 0; i < mapMatrix.Length; i++)
+            {
+                for (int j = 0; j < mapMatrix[0].Length; j++)
+                {
+                    Console.Write(mapMatrix[i][j]);
+                    if (j < mapMatrix[0].Length - 1) Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void validation()
+        {
+            Console.WriteLine(countK);
+            Console.WriteLine(countT);
+            //if (this.cols != this.rows) throw new MazeException();
+            if (this.countK != 1) throw new MazeException();
+            else if (this.countT == 0) throw new MazeException();
+        }
+    }
+
+    public class MazeException : Exception
+    {
+        public string msg() { return "Error!"; }
+    }
+}
